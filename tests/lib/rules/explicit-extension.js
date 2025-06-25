23a55637ac9cd9626574acf7565fcce3b511d03d
Bruno Oliveira <bruno.oliveira@uphold.com>
2025-06-25 11:48:04 +0100

    Add flat config support
    This adds flat config support.
    Also, the project was updated to use eslint v9
    with flat config.

--- tests/lib/rules/explicit-extension.js
+++ tests/lib/rules/explicit-extension.js
@@ -5,7 +5,7 @@ const rule = require('../../../lib/rules/explicit-extension');
 const RuleTester = require('eslint').RuleTester;
 
 const ruleTester = new RuleTester({
-  parserOptions: {
+  languageOptions: {
     ecmaVersion: 2020,
     sourceType: 'module'
   }
@@ -16,7 +16,7 @@ ruleTester.run('explicit-extension', rule, {
     {
       name: 'import relative file with extension',
       code: `import foo from './foo.js';`,
-      filename: __filename,
+      filename: __filename
     },
     {
       name: 'ignore computed dynamic imports',
@@ -24,7 +24,7 @@ ruleTester.run('explicit-extension', rule, {
 const foo = 'foo.js';
 import(\`./lib/\${foo}\`);
 `,
-      filename: path.resolve('./foo.js'),
+      filename: path.resolve('./foo.js')
     },
     {
       name: 'ignore regular exports',
@@ -33,8 +33,8 @@ const foo = 'foo';
 export default foo;
 export { foo };
 `,
-      filename: path.resolve('./foo.js'),
-    },
+      filename: path.resolve('./foo.js')
+    }
   ],
 
   invalid: [
@@ -43,69 +43,69 @@ export { foo };
       code: `import foo from './foo';`,
       output: `import foo from './foo.js';`,
       filename: __filename,
-      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }],
+      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }]
     },
     {
       name: 'import relative file without extension, starting with ../',
       code: `import foo from '../test/lib/rules/foo';`,
       output: `import foo from '../test/lib/rules/foo.js';`,
       filename: path.resolve('./tests'),
-      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }],
+      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }]
     },
     {
       name: 'import relative directory without extension',
       code: `import lib from './lib';`,
       output: `import lib from './lib/index.js';`,
       filename: path.resolve('./foo.js'),
-      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }],
+      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }]
     },
     {
       name: 'import relative directory without extension, starting with ../',
       code: `import lib from '../lib';`,
       output: `import lib from '../lib/index.js';`,
       filename: path.resolve('./tests/foo.js'),
-      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }],
+      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }]
     },
     {
       name: 'not fixable if there is no source file name',
       code: `import foo from './foo';`,
       output: null,
-      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }],
+      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }]
     },
     {
       name: 'dynamic import relative file without extension',
       code: `import('./foo');`,
       output: `import('./foo.js');`,
       filename: __filename,
-      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }],
+      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }]
     },
     {
       name: 'dynamic import relative directory without extension',
       code: `import('./lib');`,
       output: `import('./lib/index.js');`,
       filename: path.resolve('./foo.js'),
-      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }],
+      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }]
     },
     {
       name: 'export relative file without extension',
       code: `export { foo } from './foo';`,
       output: `export { foo } from './foo.js';`,
       filename: __filename,
-      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }],
+      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }]
     },
     {
       name: 'export default relative file without extension',
       code: `export { default } from './foo';`,
       output: `export { default } from './foo.js';`,
       filename: __filename,
-      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }],
+      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }]
     },
     {
       name: 'export all relative file without extension',
       code: `export * from './foo';`,
       output: `export * from './foo.js';`,
       filename: __filename,
-      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }],
-    },
-  ],
+      errors: [{ message: 'Missing extension in the source path', type: 'Literal' }]
+    }
+  ]
 });
